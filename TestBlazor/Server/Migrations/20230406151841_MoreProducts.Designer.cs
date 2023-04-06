e4a744c64d9335346b82392d90958d07819d51d1
Pierluigi Secco <pierluigi.secco@we-plus.eu>
2023-04-06 17:25:04 +0200

    added more products to visualize

--- TestBlazor/Server/Migrations/20230406151841_MoreProducts.Designer.cs
+++ TestBlazor/Server/Migrations/20230406151841_MoreProducts.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TestBlazor.Server.Data;
 
@@ -10,9 +11,11 @@ using TestBlazor.Server.Data;
 namespace TestBlazor.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230406151841_MoreProducts")]
+    partial class MoreProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
